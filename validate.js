c53ee7de2fca2718f5a84223ffcbc6263f31e934
Giovanne de Luna <gikalunalticg@gmail.com>
2024-05-16 16:39:39 -0300

    feat css, html

--- validate.js
+++ validate.js
@@ -1,1 +1,6 @@
 //Seu JavaScript de validação aqui
+
+function validateForm() {
+  let name = document.forms["formContact"]["nome"].value
+  alert(`Mensagem enviada com sucesso! Obrigado por entrar em contato ${name}.`)
+}
